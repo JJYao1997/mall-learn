612eaecac380ff002b8e38f710cbf577fdbad826
JJYao1997 <50189939+JJYao1997@users.noreply.github.com>
2022-08-27 16:50:07 +0800

    init files

---
+++ mall-learn/mall-search/src/test/java/com/yaojinjian/mallsearch/MallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaojinjian.mallsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
