5d8b50438a7ba53fc481a01c69d4a3f2df072d4c
JJYao1997 <2019697894@qq.com>
2022-08-28 14:06:54 +0800

    fulfill user class

---
+++ mall-admin/src/main/java/com/yaojinjian/malladmin/MallAdminApplication.java
@@ -0,0 +1,19 @@
+package com.yaojinjian.malladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动入口
+ *
+ * @author JJYao
+ * @date 20220827
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+
+}
